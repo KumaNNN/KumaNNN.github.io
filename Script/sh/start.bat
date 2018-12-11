71c1dd9f144375be9b7f2f3b5ce9f0c9b5f5f1e7
kuma8866 <kuma8866@163.com>
2018-12-12 02:34:08 +0800

    script 4

--- Script/sh/start.bat
+++ Script/sh/start.bat
@@ -1,9 +1,48 @@
 @echo off
+:: 开启变量延迟
+setlocal enabledelayedexpansion
+
+cd %~dp0
+
+:: 判断脚本所在路径，Script\sh\或仓库根目录
 if exist init.bat (
+	REM 跳转到仓库根目录(当前脚本路径为 Script\sh\)
+	cd.. 
+	cd..  
+
+	REM 设置根目录路径
+	set root=!cd!
+	REM 设置当前脚本路径
+	set thispath=%~dp0
+
+	REM 跳转到上级目录
+	cd..
+	REM 备份目录
+	xcopy !root!\*  !root!.bak\  /eiy
+
+	REM 跳转到当前脚本路径
+	cd !thispath!
+
+	REM 调用脚本	
 	start init.bat
 ) else (
+	REM 设置根目录路径
+	set root=!cd!
+	REM 设置当前脚本路径
+	set thispath=%~dp0
+
+	REM 跳转到上级目录
+	cd..
+	REM 备份目录
+	xcopy !thispath!*  !root!.bak\  /eiy
+
+	REM 跳转到当前脚本路径
+	cd !thispath!
+		
+	REM 调用脚本
 	start Script\sh\init.bat
-	:: 删除本脚本
+	
+	REM 删除本脚本并退出
 	del %0
 	exit
 )
